c915587649610d42b7531cbd5737eaae1ecd00a8
Nestorsg2003 <nestor.sanchezgo@alumno.buap.mx>
2023-10-26 14:37:35 -0600

    Enemigo

---
+++ Plataformero/Assets/Scripts/ControladorEnemigoPequenio.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorEnemigoPequenio : MonoBehaviour
+    
+{
+    public float velocidadCaminar = 3f;
+    public float distanciaAgro = 5;
+    private GameObject heroe;
+    private Rigidbody2D miCuerpo;
+    private Animator miAnimador;
+    void Start()
+    {
+        heroe = GameObject.FindWithTag("Player");
+        miCuerpo = GetComponent<Rigidbody2D>();
+        miAnimador = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 posHeroe = heroe.transform.position;
+        Vector3 posYo = this.transform.position;
+        float distancia = (posYo - posHeroe).magnitude;
+        float velActualVert = miCuerpo.velocity.y;
+
+        if (distancia < distanciaAgro)
+        {//El heroe esta fuera de la zona de agro
+            if(posHeroe.x > posYo.x)
+            {//El heroe derecha villando
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                miCuerpo.velocity = new Vector3(velocidadCaminar, velActualVert, 0);
+                miAnimador.SetBool("caminando", true);
+            }
+            else
+            {//El heroe izquierda villando
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                miCuerpo.velocity = new Vector3(-velocidadCaminar, velActualVert, 0);
+                miAnimador.SetBool("caminando", true);
+            }
+        }
+        else
+        {//El heroe esta fuera de la zona de agro
+            miCuerpo.velocity = new Vector3(0, 0, 0);
+            miAnimador.SetBool("caminando", false);
+        }
+    }
+}
