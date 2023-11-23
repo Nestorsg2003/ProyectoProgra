dcb04c6c0c2edd68ed7b89c97164e609fe2ddfcf
Nestorsg2003 <142053667+Nestorsg2003@users.noreply.github.com>
2023-11-23 00:22:29 -0600

    Revert "MONEDA"
    This reverts commit 4ebff62edcf7ef0e9b5be607267b6e12aa50d86a.

--- Plataformero/Assets/Scripts/TesoroMoneda.cs
+++ Plataformero/Assets/Scripts/TesoroMoneda.cs
@@ -4,31 +4,15 @@ using UnityEngine;
 
 public class TesoroMoneda : MonoBehaviour
 {
-    private Animator miAnimador;
-    public float monedas = 0;
-    public Personaje heroe;
     // Start is called before the first frame update
     void Start()
     {
-        miAnimador = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    private void OnTriggerEnter2D(Collider2D agarrar)
-    {
-        TesoroMoneda moneda = GetComponent<TesoroMoneda>();
-        print(name + "hizo colision con" + agarrar.gameObject.name);
-        GameObject otro = agarrar.gameObject;
-        if (otro.tag == "Player")
-        {
-            heroe.score ++;
-            monedas ++;
-            miAnimador.SetTrigger("OBTENER");
-            Destroy(gameObject, 1);
-        }
     }
 }
